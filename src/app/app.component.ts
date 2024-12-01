39d7d8c383334e2a18e78d1856ea7157abdf68a5
your-username <mahmoudabdelaziz93.mz@gmail.com>
2024-12-01 18:24:54 +0200

    feat: update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
+import { User, users } from './../DUMMY-USERS';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo-list-angular';
+  selectedUserId: number | undefined;
+  allUsers: User[] = users;
+select: any;
+  onSelectUser(id: number) {
+    this.selectedUserId = id;
+  }
 }
